567390d773a011ddc12b6969e36fa6978f2d3a8b
be_cracked <becracked@googlemail.com>
2019-12-29 13:31:54 +0100

    Add stopwatch arg

--- Visualiser/Program.cs
+++ Visualiser/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 using System.Drawing;
 using System.Drawing.Imaging;
 using CommandLine;
@@ -7,14 +9,19 @@ namespace Visualiser
 {
     static class Program
     {
+        [SuppressMessage("ReSharper", "UnusedAutoPropertyAccessor.Local")]
         public class Options
         {
             [Option('f', "filePath", Required = true, HelpText = "Set the file to save to.")]
-            public string FilePath { get; set; }
+            public string FilePath { get; private set; }
 
             [Option('r', "resPow", Required = false, Default = 12,
                 HelpText = "The power to raise 2 to to calculate the resolution.")]
-            public int ResPow { get; set; }
+            public int ResPow { get; private set; }
+
+            [Option('t', "trackTime", Required = false, Default = false,
+                HelpText = "Whether to stop the time it takes to generate an image.")]
+            public bool TrackTime { get; set; }
         }
 
         static void Main(string[] args)
@@ -26,6 +33,10 @@ namespace Visualiser
             int width = 0;
             int height = 0;
 
+            // Time Tracking
+            bool trackTime = false;
+            Stopwatch stopwatch = new Stopwatch();
+
             Parser.Default.ParseArguments<Options>(args).WithParsed<Options>(o =>
             {
                 file = o.FilePath;
@@ -33,14 +44,29 @@ namespace Visualiser
                 width = (int) Math.Pow(2, o.ResPow);
                 height = (int) Math.Pow(2, o.ResPow);
 
+                trackTime = o.TrackTime;
             });
 
             Console.WriteLine($"Generating {width}x{height} Mandelbrot canvas.");
 
+            if (trackTime)
+            {
+                stopwatch.Start();
+            }
+
             MandelbrotGenerator mandelbrotGenerator = new MandelbrotGenerator(width, height);
 
             Image img = mandelbrotGenerator.GetCanvas();
-            Console.WriteLine("Rendered canvas!");
+
+            if (trackTime)
+            {
+                stopwatch.Stop();
+                Console.WriteLine($"Rendered canvas in {stopwatch.Elapsed}!");
+            }
+            else
+            {
+                Console.WriteLine($"Rendered canvas!");
+            }
 
             img.Save(file, ImageFormat.Png);
             Console.WriteLine($"Saved image to '{file}'.");
