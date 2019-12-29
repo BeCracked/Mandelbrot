5f63fbda1cecb97f9f0ba8b8df5242bd5e1a6806
be_cracked <becracked@googlemail.com>
2019-12-29 13:11:45 +0100

    Add commandline args

--- Visualiser/Program.cs
+++ Visualiser/Program.cs
@@ -1,19 +1,41 @@
 ﻿using System;
 using System.Drawing;
 using System.Drawing.Imaging;
+using CommandLine;
 
 namespace Visualiser
 {
     static class Program
     {
+        public class Options
+        {
+            [Option('f', "filePath", Required = true, HelpText = "Set the file to save to.")]
+            public string FilePath { get; set; }
+
+            [Option('r', "resPow", Required = false, Default = 12,
+                HelpText = "The power to raise 2 to to calculate the resolution.")]
+            public int ResPow { get; set; }
+        }
+
         static void Main(string[] args)
         {
-            // File to save to
-            string file = "/home/be_cracked/dev/test/mandeln.png";
+            // File path to save image to
+            string file;
 
             // image parameters
-            int width = (int) Math.Pow(2,12);
-            int height = (int) Math.Pow(2,12);
+            int width = 0;
+            int height = 0;
+
+            Parser.Default.ParseArguments<Options>(args).WithParsed<Options>(o =>
+            {
+                file = o.FilePath;
+
+                width = (int) Math.Pow(2, o.ResPow);
+                height = (int) Math.Pow(2, o.ResPow);
+            });
+            // File to save to
+            file = "/home/be_cracked/dev/test/mandeln.png";
+
 
             Console.WriteLine($"Generating {width}x{height} Mandelbrot canvas.");
 
